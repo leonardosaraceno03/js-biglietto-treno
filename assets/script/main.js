ab923f2b912e660e1f1d0e47678162a0eb2aa37c
LeonardoSaraceno <leonardosaraceno03@gmail.com>
2022-10-19 16:54:31 +0200

    correzione problema

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,15 @@
 function generaBiglietto(){
+    console.log('ciao')
     let UserKm= document.getElementById('InputUserKm').value;
     let UserAge= document.getElementById('InputUserAge').value;
-    document.getElementById('info').innerHTML = `info : ${UserKm + UserAge}`
+    
+    if(UserAge < 18){
+        document.getElementById('info').innerHTML = `sei bimbo: `
+    }
+    else if(UserAge > 65){
+        document.getElementById('info').innerHTML = `sei vecchio: `
+    }
+    else{
+        document.getElementById('info').innerHTML = `sei normale: `
+    }
 }
- 
